792098e6adc5dc3ab33429a184e91c1fd0e9c4c9
{miyu-y} <{2002miyuu0514@gmail.com}>
2025-02-24 19:03:20 +0900

    check improve

--- create_dataset.py
+++ create_dataset.py
@@ -66,3 +66,53 @@ with open("dataset/rag_truth_dev.json", "w") as f:
     json.dump(dev_data, f, indent=4)
 with open("dataset/rag_truth_test.json", "w") as f:
     json.dump(test_data, f, indent=4)
+
+# span
+train_data = []
+dev_data = []
+test_data = []
+for id in id_list:
+    # document
+    for d in info_data[:]:
+        if d["source_id"] == id:
+            task_type = d["task_type"]
+            source = d["source"]
+            ref = d["source_info"]
+            info_data.remove(d)
+            break
+    # text
+    num = 0
+    for d2 in res_data[:]:
+        if num == 6:
+            break
+        if d2["source_id"] == id:
+            num += 1
+            model_name = d2["model"]
+            text = d2["response"]
+            labels = 0 if d2["labels"] == [] else 1
+            data = {
+                "ref": str(ref),
+                "text": text,
+                "labels": labels,
+                "hallucination_id": d2["labels"],  # location
+                "hallucination": {"hallucination_list": [x["text"] for x in d2["labels"]]},
+                "source": source,  # name of dataset
+                "model": model_name,
+                "task_type": task_type,
+                "source_id": id,
+            }
+            if d2["split"] == "train":
+                if id in id_dev:
+                    dev_data.append(data)
+                else:
+                    train_data.append(data)
+            else:
+                test_data.append(data)
+            res_data.remove(d2)
+
+with open("dataset/rag_truth_span_train.json", "w") as f:
+    json.dump(train_data, f, indent=4)
+with open("dataset/rag_truth_span_dev.json", "w") as f:
+    json.dump(dev_data, f, indent=4)
+with open("dataset/rag_truth_span_test.json", "w") as f:
+    json.dump(test_data, f, indent=4)
